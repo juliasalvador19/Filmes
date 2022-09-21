398c55f817dc79e25b5b9bee7a722fa98b28b666
juliasalvador19 <juliasalvador10@gmail.com>
2022-09-21 20:45:52 -0300

    Atividades de Programação Web I

--- ALTERAR/alterar_filmes.php
+++ ALTERAR/alterar_filmes.php
@@ -17,8 +17,7 @@ $row_filme = mysqli_fetch_assoc($resultado_filmes);
     <title>Alterar filmes</title>
 
     <!-- Links -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" 
-        rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style_alterar.css">
 </head>
 <body>
